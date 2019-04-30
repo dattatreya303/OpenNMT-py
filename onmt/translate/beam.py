e7ed47deb216b0c52115547f40c70cbc2fb8dd96
Sebastian Gehrmann <s.gehrmann@outlook.com>
2019-04-30 16:44:55 -0400

    .

--- onmt/translate/beam.py
+++ onmt/translate/beam.py
@@ -24,7 +24,11 @@ class Beam(object):
                  stepwise_penalty=False,
                  block_ngram_repeat=0,
                  exclusion_tokens=set(),
+<<<<<<< HEAD:onmt/translate/beam.py
                  vocab={}):
+=======
+                 dot_token=None):
+>>>>>>> copy_constraint:onmt/translate/Beam.py
 
         self.size = size
         self.tt = torch.cuda if cuda else torch
@@ -44,6 +48,9 @@ class Beam(object):
         # Has EOS topped the beam yet.
         self._eos = eos
         self.eos_top = False
+
+        # Token that ends sentences
+        self._dot = dot_token
 
         # The attentions (matrix) for each time.
         self.attn = []
@@ -161,8 +168,21 @@ class Beam(object):
                             fail = True
                             break
                         ngrams.add(tuple(gram))
+<<<<<<< HEAD:onmt/translate/beam.py
                 if fail:
                     beam_scores[j] = -10e20
+=======
+                    if fail:
+                        beam_scores[j] = -10e20
+            #sentence blocker
+            le = len(self.next_ys)
+            for j in range(self.next_ys[-1].size(0)):
+                if self.next_ys[-1][j] == self._dot:
+                    hyp, _ = self.get_hyp(le-1, j)
+                    num_sents = sum([1 for t in hyp if t==self._dot]) 
+                    if num_sents < 3:
+                        word_probs[j][self._eos] = -1e20 
+>>>>>>> copy_constraint:onmt/translate/Beam.py
         else:
             # Reduce probability of starting with I by some %
             word_probs[0][self.vocab["I"]] *= 0.7
@@ -184,9 +204,12 @@ class Beam(object):
 
         for i in range(self.next_ys[-1].size(0)):
             if self.next_ys[-1][i] == self._eos:
-                global_scores = self.global_scorer.score(self, self.scores)
-                s = global_scores[i]
-                self.finished.append((s, len(self.next_ys) - 1, i))
+                hyp, _ = self.get_hyp(len(self.next_ys)-1, i)
+                num_sents = sum([1 for t in hyp if t==self._dot])
+                if num_sents > 2:
+                    global_scores = self.global_scorer.score(self, self.scores)
+                    s = global_scores[i]
+                    self.finished.append((s, len(self.next_ys) - 1, i))
 
         # End condition is when top-of-beam is EOS and no global score.
         if self.next_ys[-1][0] == self._eos:
